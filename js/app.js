e04514c0983af4f07a0c546945fc525203beb4be
Elzbeth Blanco <elzbethblanco@gmail.com>
2018-01-18 15:43:20 -0600

    commit to branch

--- js/app.js
+++ js/app.js
@@ -1,24 +1,22 @@
 
   //Traer elementos del HTML
-  
+  var $nameInput = $('#name-input');
+  var $phoneInput = $('#phone-input');
   
   var loadPage = function () {
     
+ $('.modal').modal();
+ $nameInput.keyup(validateContact);
+ $phoneInput.keyup(validateContact);
+
   };
 
-  var paintContactsInHTML = function(contact) {
-   
-    /* Crear elementos con DOM html al publicar contacto */
-     
-    
-    //Asignando atributos y/o eventos
-    
-
-    /* Asignando valores a los elementos*/
-    
-
-    //Agregamos lo que creamos con el DOM a un elemento existente del HTML
-    
+  var validateContact = function () {
+    //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
+    var $containerAddContact = $("#add-contact");
+    if($(this).val().trim().length > 0){
+      $("#add-contact").attr("disbled",true);
+    }
   };
 
   var addContact = function (e) {
@@ -37,11 +35,20 @@
     
   };
 
-  
 
+  var paintContactsInHTML = function(contact) {
+   
+    /* Crear elementos con DOM html al publicar contacto */
+     
+    
+    //Asignando atributos y/o eventos
+    
 
-  var validateContact = function () {
-    //En esta funcion tenemos que validar que el usuario ingrese datos y no valores vacios
+    /* Asignando valores a los elementos*/
+    
+
+    //Agregamos lo que creamos con el DOM a un elemento existente del HTML
+    
   };
 
 
